40987f5e12064e1cd9ae57faa57d38d7fb49a45d
Thoriq-ha <ithoriq0@gmail.com>
2022-09-23 06:02:02 +0700

    bbm1

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rameneapps/data_foto.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,7 +12,76 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Kita akan membuat sebuah aplikasi')),
+      appBar: AppBar(
+        title: const Text(
+          'Home',
+          style: TextStyle(color: Colors.black),
+        ),
+        actions: [
+          InkWell(
+            onTap: (() {}),
+            child: Padding(
+              padding: const EdgeInsets.only(right: 22),
+              child: Image.asset(
+                'assets/ic_notification.png',
+                height: 24,
+                width: 24,
+              ),
+            ),
+          ),
+        ],
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Search",
+                  contentPadding:
+                      const EdgeInsets.only(left: 20.0, right: 10.0),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(24),
+                      borderSide: const BorderSide(color: Colors.amber)),
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(24),
+                      borderSide: const BorderSide(color: Colors.amber)),
+                ),
+              )),
+          const SizedBox(
+            height: 12,
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Text(
+              "Top Menu",
+              textAlign: TextAlign.start,
+              style: TextStyle(color: Colors.black, fontSize: 20),
+            ),
+          ),
+          Expanded(
+            child: GridView.builder(
+                itemCount: dataFoto.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                ),
+                itemBuilder: ((context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset(
+                      'assets/${dataFoto[index]}',
+                      height: 150,
+                      width: 150,
+                    ),
+                  );
+                })),
+          )
+        ],
+      ),
     );
   }
 }
